bde0926127fc4fc6cd2309ac3a79c3714966406e
llhosk <138557428+llhosk123@users.noreply.github.com>
2026-02-08 17:46:57 +0900

    Update admin passwords to use SHA2 hashing

--- temp-web/admin-test/db/init.sql
+++ temp-web/admin-test/db/init.sql
@@ -8,5 +8,16 @@ CREATE TABLE IF NOT EXISTS admins (
 );
 
 INSERT INTO admins (admin_id, admin_pw) VALUES
-('admin01', 'Init@Admin01!'),
-('admin02', 'Init@Admin02!');
+('admin', SHA2('123456', 256)),
+('master', SHA2('234561', 256)),
+('leader', SHA2('345612', 256)),
+('security', SHA2('456123', 256)),
+('db_admin', SHA2('561234', 256)),
+('k3s_admin', SHA2('543216', 256));
+('infra_admin', SHA2('654321', 256)),
+('db_manager', SHA2('246135', 256)),
+('k3s_manager', SHA2('612345', 256)),
+('infra_manager', SHA2('135246', 256)),
+
+
+

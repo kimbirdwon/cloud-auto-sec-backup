167a9a93288ba95f4cd42e3c687a5cdb1d35ab45
Sewon Kim <pswk0907@gmail.com>
2026-02-10 19:00:45 +0900

    변수 버전 수정

--- terraform/ami.tf
+++ terraform/ami.tf
@@ -1,13 +1,9 @@
-/*
-  AMI 데이터 소스 및 AWS Provider 설정
-*/
-# 자동으로 아마존 리눅스2023 AMI 최신 이미지 가져오기
-data "aws_ami" "amazon_linux_2023" {                      # data → 기존 리소스를 참조
-  most_recent = true                                     # 최신 AMI 선택
-  owners      = ["amazon"]                               # 공식 Amazon 계정 AMI만 선택
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = var.ami_owners
 
   filter {
-    name   = "name"                                # 이름 기준 필터
-    values = ["al2023-ami-*-x86_64"]               # Amazon Linux 2023, 64비트
+    name   = "name"
+    values = [var.ami_name_pattern]
   }
 }

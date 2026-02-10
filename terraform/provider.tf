c983a54ae96404483dc6400f873e34a9194f40e3
Sewon Kim <pswk0907@gmail.com>
2026-02-10 18:56:41 +0900

    변수 버전 수정

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,5 +4,6 @@
 
 # AWS Provider 설정
 provider "aws" {		                               # provider → Terraform과 AWS 연결
-  region = "ap-northeast-2"                        # 서울 리전 선택
+  region = var.aws_region                        # 서울 리전 선택
 }
+

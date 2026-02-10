a088fc898e21bef526b2427bb0bc14881d3f416f
haseolc <haseok612@naver.com>
2026-02-10 19:13:45 +0900

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,3 @@
-/* 
-  AWS Provider 설정
-*/
-
-# AWS Provider 설정
-provider "aws" {		                               # provider → Terraform과 AWS 연결
-  region = var.aws_region                        # 서울 리전 선택
+provider "aws" {
+  region = var.aws_region
 }
-

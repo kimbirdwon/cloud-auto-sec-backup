f35749509895bfafa5e96b0d74ca82a5981f68eb
Sewon Kim <pswk0907@gmail.com>
2026-02-10 22:11:43 +0900

    주석 수정

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,7 +6,7 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# Default VPC 내에서 첫 번째 AZ의 public subnet을 선택 (public IP 자동 할당되는 서브넷 기대)
+# AZ a에 있는 public subnet만 조회
 data "aws_subnets" "public_az_a" {
   filter {
     name   = "vpc-id"

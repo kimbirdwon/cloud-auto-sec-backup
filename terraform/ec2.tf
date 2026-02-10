95aff34653075c2b8fce7ec598fa40d8e15228fd
haseolc <haseok612@naver.com>
2026-02-10 10:03:13 +0900

    Refactor EC2 resource to use variable security group IDs
    Updated security group references to use variables and added variable definitions.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "AWS_ec2_sg_7th_room" {
   instance_type = "t3.small"                             # 인스턴스 타입
   subnet_id     = data.aws_subnets.default.ids[0]        # 퍼블릭 서브넷
   associate_public_ip_address = true                     # Public IP 자동 할당
-  vpc_security_group_ids = [aws_security_group.sg_7th_room.id, aws_security_group.web_sg_7th_room.id]   # 보안 그룹 적용
+  vpc_security_group_ids = [var.sg_7th_room_id, var.web_sg_7th_room_id]   # 보안 그룹 적용
   key_name      = "infra-dev-key"                           # 키페어 이름
   #infra-dev-key
   tags = {
@@ -14,3 +14,10 @@ resource "aws_instance" "AWS_ec2_sg_7th_room" {
   }
 }
 
+variable "sg_7th_room_id" {
+  type = string
+}
+
+variable "web_sg_7th_room_id" {
+  type = string
+}

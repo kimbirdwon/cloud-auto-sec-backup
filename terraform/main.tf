d074f54bef6479306e028cbf1be5605691982b45
Sewon Kim <pswk0907@gmail.com>
2026-02-11 09:21:28 +0900

    퍼블릭 지정 코드 제거

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,6 @@ data "aws_security_group" "web_sg" {
 resource "aws_instance" "AWS_ec2_sg_7th_room" {
   ami                         = var.ami_id # data.aws_ami.amazon_linux_2023.id
   instance_type               = var.instance_type
-  associate_public_ip_address = var.associate_public_ip
   key_name                    = var.key_pair_name
 
   subnet_id = data.aws_subnets.public_az_a.ids[0]
@@ -47,3 +46,4 @@ resource "aws_instance" "AWS_ec2_sg_7th_room" {
     Project     = "infra-auto"
   }
 }
+

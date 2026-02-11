289559f84618954bbecc97cccf8a3b79f86bdb62
Sewon Kim <pswk0907@gmail.com>
2026-02-11 09:37:15 +0900

    프로젝트 태그도 변수화

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,7 +43,8 @@ resource "aws_instance" "AWS_ec2_sg_7th_room" {
   tags = {
     Name        = var.instance_name
     Environment = var.environment
-    Project     = "infra-auto"
+    Project     = var.project
   }
 }
+
 

23fcaaf9fdb0ccbb5e0d32b04f516f40ce8485d9
Sewon Kim <pswk0907@gmail.com>
2026-02-21 21:50:46 +0900

    오타 수정

--- rds.tf
+++ rds.tf
@@ -23,7 +23,7 @@ data "aws_db_snapshot" "latest_automated" {
 # DR용 RDS 스냅샷 복구
 resource "aws_db_instance" "db_7th_room_dr" {
   identifier             = "rds-7th-room-dr"
-  snapshot_identifier    = data.aws_db_snapshot.latest_automated.id # 스냅샷 복구
+  snapshot_identifier    = data.aws_db_snapshot.latest_automated.id
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "mysql"
@@ -33,8 +33,8 @@ resource "aws_db_instance" "db_7th_room_dr" {
   username               = "admin"
   password               = var.db_password
 
-  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = data.aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [data.aws_security_group.rds_sg.id]
 
   multi_az                = false
   availability_zone       = "ap-northeast-2a"
